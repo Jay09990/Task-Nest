db38f497dd6aa7e8184d3e3e430f1b167b9c055a
Jay09990 <jdudhrejiya0990@gmail.com>
2025-07-09 23:48:44 +0530

    feat: Add loading state handling and fix user name rendering in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import { Bell, LogOut, UserPlus, LogIn } from "lucide-react";
 import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
-  const { user, logout } = useAuth();
+  const { user, logout, isLoading } = useAuth();
+  if (isLoading) return null;
 
   return (
     <div className="h-16 bg-white border-b border-gray-200 flex items-center justify-between px-6 shadow-sm">
@@ -11,13 +12,19 @@ const Navbar = () => {
 
       {/* Right: Actions */}
       <div className="flex items-center space-x-4">
+        {/* {user ? console.log("getting user") : console.log("not getting user")}; */}
+        
+        {/* TODO: Fix user specific content rendering */}
         {user ? (
           // Logged-in: Show welcome message and logout
           <div className="flex items-center space-x-4">
             {/* Welcome Message */}
             <div className="text-sm text-gray-600">
               Welcome,{" "}
-              <span className="font-semibold text-gray-800">{user.name}</span>!
+              <span className="font-semibold text-gray-800">
+                {user.userName}
+              </span>
+              !
             </div>
 
             {/* Logout Button */}
